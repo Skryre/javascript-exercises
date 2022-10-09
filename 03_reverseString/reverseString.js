990b6b601c5c6d2ea111d98d4bb60a257642a67b
Skryre <zbk1998@ymail.com>
2022-10-09 18:07:06 -0400

    Just doing the exercices

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let string = '';
 
+    let sentence = word;
+
+    let reversed= sentence.split('').reverse().join('');
+
+    string += reversed;
+
+    return string;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+//Solution
+//const reverseString = function(string) {
+// return string.split('').reverse().join('');
+//};
